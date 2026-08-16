8791f6e952b4466ae17253f133c260e8e593d2dd
蒙维佳 <higher@MW-2.local>
2026-08-16 18:09:21 +0800

    Improve explanations for first 200 advanced PDF questions

--- scripts/build_standalone.py
+++ scripts/build_standalone.py
@@ -13,7 +13,7 @@ INDEX = ROOT / "index.html"
 QUESTIONS = ROOT / "data" / "questions.js"
 ICON = ROOT / "icon.svg"
 CORE = ROOT / "quiz-core.js"
-OUTPUT = ROOT / "dist" / "电力交易员中级工高级工技师刷题_单文件版_v22.html"
+OUTPUT = ROOT / "dist" / "电力交易员中级工高级工技师刷题_单文件版_v23.html"
 
 
 SCRIPT_BLOCK_RE = re.compile(
